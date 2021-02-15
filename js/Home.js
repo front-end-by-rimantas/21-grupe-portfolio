ce2b2c2976bc39d430ca426e7196b8ca0dd1f8c7
Rimantas <belauzas@yahoo.com>
2021-02-15 18:45:40 +0200

    Home page comments

---
+++ js/Home.js
@@ -0,0 +1,31 @@
+// IMPORTS
+/* header */
+/* hero */
+/* about me */
+/* hobbies */
+/* achievements */
+/* services */
+/* resume */
+/* expertise */
+/* portfolio */
+/* testimonials */
+/* blog */
+/* freelance */
+/* contact me */
+/* footer */
+
+// EXECUTION
+/* header */
+/* hero */
+/* about me */
+/* hobbies */
+/* achievements */
+/* services */
+/* resume */
+/* expertise */
+/* portfolio */
+/* testimonials */
+/* blog */
+/* freelance */
+/* contact me */
+/* footer */
