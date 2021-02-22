64f55e72fad65c336f98a9de485426ff7ee55135
Rimantas <belauzas@yahoo.com>
2021-02-22 19:45:18 +0200

    Hobbies section #12

--- js/components/hobbies/hobbies.js
+++ js/components/hobbies/hobbies.js
@@ -1,7 +1,29 @@
 function hobbies(data) {
     // validation
-    if (!data) {
-        return false;
+    if (data === undefined) {
+        return 'Neduoti parametrai';
+    }
+    if (typeof data !== 'object' || Array.isArray(data)) {
+        return 'Duotas netinkamo tipo parametras';
+    }
+
+    const allowedKeys = ['selector', 'list'];
+    const keys = Object.keys(data);
+    if (keys.length !== allowedKeys.length) {
+        return 'Parametre yra netinkamas raktazodziu kiekis';
+    }
+    for (const key of keys) {
+        if (!allowedKeys.includes(key)) {
+            return 'Parametro struktura neteisinga';
+        }
+    }
+
+    if (typeof data.selector !== 'string' || data.selector === '') {
+        return 'Selectorius netinkamo formato';
+    }
+
+    if (!Array.isArray(data.list) || data.list.length === 0) {
+        return 'List netinkamo formato';
     }
 
     // logic
@@ -10,16 +32,32 @@ function hobbies(data) {
 
     let HTML = '';
     for (const item of list) {
-        HTML += `<div class="col-12 col-md-4 col-lg-3 hobbie">
-                    <div class="icon">${item.icon}</div>
-                    <p class="text">${item.text}</p>
-                </div>`;
+        if (
+            typeof item === 'object' &&
+            !Array.isArray(item) &&
+            item.icon &&
+            typeof item.icon === 'string' &&
+            item.icon !== '' &&
+            item.text &&
+            typeof item.text === 'string' &&
+            item.text !== ''
+        ) {
+            HTML += `<div class="col-12 col-md-4 col-lg-3 hobbie">
+                        <div class="icon">${item.icon}</div>
+                        <p class="text">${item.text}</p>
+                    </div>`;
+        }
     }
 
     // post logic validation
+    if (HTML === '') {
+        return 'Parametruose nebuvo jokios naudingos/teisingos informacijos';
+    }
 
     // result return
     DOM.innerHTML = HTML;
+
+    return true;
 }
 
-export { hobbies }
+export { hobbies };
--- js/components/hobbies/hobbies.test.js
+++ js/components/hobbies/hobbies.test.js
@@ -1,11 +1,55 @@
 import { hobbies } from './hobbies.js';
 
-describe('Testai su negiamu rezultatu', () => {
+describe('Tikramas parametro formatas', () => {
     test('neduota parametru', () => {
-        expect(hobbies()).toBeFalsy();
+        expect(hobbies()).toBe('Neduoti parametrai');
     });
 
     test('duotas netinkamo tipo parametras: skaicius', () => {
-        expect(hobbies(5)).toBeFalsy();
+        expect(hobbies(5)).toBe('Duotas netinkamo tipo parametras');
     });
-})
+    test('duotas netinkamo tipo parametras: array', () => {
+        expect(hobbies([])).toBe('Duotas netinkamo tipo parametras');
+    });
+});
+
+describe('Parametro raktazodziu formatas', () => {
+    test('tuscias objektas yra negalimas', () => {
+        expect(hobbies({})).toBe('Parametre yra netinkamas raktazodziu kiekis');
+    });
+    test('nepakanka objekte raktazodziu', () => {
+        expect(hobbies({ selector: '#hobbies' })).toBe(
+            'Parametre yra netinkamas raktazodziu kiekis'
+        );
+    });
+    test('objektas sudarytas is netinkamu raktazodziu', () => {
+        expect(hobbies({ selector: '#hobbies', school: 'name' })).toBe(
+            'Parametro struktura neteisinga'
+        );
+    });
+    test('objektas sudarytas is netinkamu raktazodziu', () => {
+        expect(
+            hobbies({ selector: '#hobbies', list: [], school: 'name' })
+        ).toBe('Parametre yra netinkamas raktazodziu kiekis');
+    });
+    test('selectorius turi buti ne tuscias tekstas', () => {
+        expect(hobbies({ selector: 5, list: [] })).toBe(
+            'Selectorius netinkamo formato'
+        );
+    });
+    test('selectorius turi buti ne tuscias tekstas', () => {
+        expect(hobbies({ selector: '', list: [] })).toBe(
+            'Selectorius netinkamo formato'
+        );
+    });
+    test('list turi buti ne tuscias array', () => {
+        expect(hobbies({ selector: '#hobbies', list: 5 })).toBe(
+            'List netinkamo formato'
+        );
+    });
+    test('list turi buti ne tuscias array', () => {
+        expect(hobbies({ selector: '#hobbies', list: [] })).toBe(
+            'List netinkamo formato'
+        );
+    });
+});
