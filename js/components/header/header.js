76b7c7ce0ebb62003ef6b8fcb82bd3192201b951
Rimantas <belauzas@yahoo.com>
2021-03-01 20:14:56 +0200

    Hamburger mobile menu

--- js/components/header/header.js
+++ js/components/header/header.js
@@ -1,5 +1,6 @@
 export default () => {
     const header = document.querySelector('header');
+    const hamburger = header.querySelector('.fa-bars');
     const headerScrollBreakPointInPixels = 40;
 
     window.addEventListener('scroll', () => {
@@ -8,5 +9,9 @@ export default () => {
         } else {
             header.classList.remove('scroll');
         }
+    });
+
+    hamburger.addEventListener('click', () => {
+        header.classList.toggle('expand');
     });
 };
